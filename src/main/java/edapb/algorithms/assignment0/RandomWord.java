e12da46430902c548133294cb29fd44d79fefdcb
Elian <elian.ddb@outlook.com>
2022-05-19 01:30:57 -0400

    added RandomWord.java

---
+++ src/main/java/edapb/algorithms/assignment0/RandomWord.java
@@ -0,0 +1,11 @@
+package edapb.algorithms.assignment0;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        while (StdIn.isEmpty()) {
+            // ...
+        }
+    }
+}
