f6416de765a0e7db16225b4b14c7a9eff1c741d6
Elian <elian.ddb@outlook.com>
2022-05-23 19:57:30 -0400

    made grid accessible to whole class

--- src/main/java/edapb/algorithms/assignment1/Percolation.java
+++ src/main/java/edapb/algorithms/assignment1/Percolation.java
@@ -3,9 +3,12 @@ package edapb.algorithms.assignment1;
 public class Percolation {
     private final int closed = 0;
     private final int open = 1;
+
+    private int[][] grid;
     public Percolation(final int n) {
-        int[][] grid = new int[n][n];
+        grid = new int[n][n];
     }
 
-
+    public void open(int row, int col) {
+    }
 }
