d2ca86df986204e9945a487d813fd58f4130565d
Elian <elian.ddb@outlook.com>
2022-05-23 20:00:30 -0400

    added width and length variables

--- src/main/java/edapb/algorithms/assignment1/Percolation.java
+++ src/main/java/edapb/algorithms/assignment1/Percolation.java
@@ -3,12 +3,17 @@ package edapb.algorithms.assignment1;
 public class Percolation {
     private final int closed = 0;
     private final int open = 1;
+    private final int width;
+    private final int height;
 
     private int[][] grid;
     public Percolation(final int n) {
         grid = new int[n][n];
+        width = n;
+        height = n;
     }
 
     public void open(int row, int col) {
+        grid[row][col] = open;
     }
 }
