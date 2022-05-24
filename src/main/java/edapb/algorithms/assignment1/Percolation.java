e65d5c305ffceee2e452a8cc1ccfcc5964a30aaa
Elian <elian.ddb@outlook.com>
2022-05-23 21:05:48 -0400

    switched to a single dimensional array to hold values

--- src/main/java/edapb/algorithms/assignment1/Percolation.java
+++ src/main/java/edapb/algorithms/assignment1/Percolation.java
@@ -1,38 +1,46 @@
 package edapb.algorithms.assignment1;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
     private final int full = 0;
     private final int open = 1;
     private final int width;
     private final int height;
 
-    private int[][] grid;
+    private WeightedQuickUnionUF ids;
+    private int[] status;
+
     public Percolation(final int n) {
         if (n <= 0) {
             throw new IllegalArgumentException();
         }
 
-        grid = new int[n][n];
+        // Using one dimensional array as grid.
+        final int size = n*n;
+        ids = new WeightedQuickUnionUF(size);
+        status = new int[size];
         width = n;
         height = n;
     }
 
-    private void enforceRange(int row, int col) {
+    private int toIndex(int row, int col) {
         if (row <= 0 || row > height || col <= 0 || col >= width) {
             throw new IllegalArgumentException();
         }
+        // First element is at (1, 1).
+        // We do -1 to maintain ourselves on the first row when row 1 is asked for.
+        return (row - 1) * width + col;
     }
-
     public void open(int row, int col) {
-        enforceRange(row, col);
-        grid[row][col] = open;
+        status[toIndex(row, col)] = open;
     }
 
     public boolean isOpen(int row, int col) {
-        return grid[row][col] == open;
+        return status[toIndex(row, col)] == open;
     }
 
     public boolean isFull(int row, int col) {
-        return grid[row][col] == full;
+        return status[toIndex(row, col)] == full;
     }
 }
