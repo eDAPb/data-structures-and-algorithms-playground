31cbb1e7c436c463b91c9a40fcf3e6f2b577fa6e
Elian <elian.ddb@outlook.com>
2022-05-26 17:34:02 -0400

    implemented backwash fix with bitwise operations, relies on the roots of trees to hold the status of nodes

--- src/main/java/edapb/algorithms/assignment1/Percolation.java
+++ src/main/java/edapb/algorithms/assignment1/Percolation.java
@@ -3,36 +3,30 @@ package edapb.algorithms.assignment1;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private static final byte CLOSE = 0b0000; // 0
-    private static final byte OPEN = 0b0001; // 1
-    private static final byte TOP_CONN = 0b0010; // 2
-    private static final byte BOTTOM_CONN = 0b0100; // 4
-    private static final byte PERCOLATION = 0b0111; // 7
+    private static final int OPEN = 0b100;
+    private static final int TOP_CONN = 0b110;
+    private static final int BOTTOM_CONN = 0b101;
+    private static final int PERCOLATION = 0b0111;
+
     private final int sideLength;
-    private final int topNode;
+    private final int nullNode;
 
     private final WeightedQuickUnionUF idsUF;
-    private final byte[] status;
-    private int numOfOpenSites;
+    private final int[] status;
     private boolean percolates;
+    private int numOfOpenSites;
 
     public Percolation(final int n) {
         if (n <= 0) {
             throw new IllegalArgumentException();
         }
 
-        // Using one dimensional array as grid.
+        // Using one dimensional array as grid. min (1, 1) max (n, n)
         final int size = n * n + 1;
         sideLength = n;
+        nullNode = 0;           // Serves as dummy node for incorrect access
         idsUF = new WeightedQuickUnionUF(size);
-        status = new byte[size];
-        // We will track opened bottom nodes and check if they are full to
-        // confirm for percolation later. First index reserved for counting.
-        numOfOpenSites = 0;
-
-        // Spawn top virtual node.
-        topNode = 0;
-        status[topNode] = TOP_CONN;
+        status = new int[size]; // Keep nullNode status as CLOSED = 0b000
     }
 
     private boolean inRange(int row, int col) {
@@ -45,26 +39,24 @@ public class Percolation {
         }
     }
     private int toIndex(int row, int col) {
-        // Lets us internally access the top node.
-        if (row == 0) {
-            return topNode;
-        }
-
         if (!inRange(row, col)) {
-            return -1;
+            return nullNode;
         }
 
-        // First element is at (1, 1) -> arr[1]
-        // We do -1 to maintain ourselves on the first row when row 1 is asked for.
-        return (row - 1) * sideLength + col; // separate
+        // We subtract 1 to get the correct index, row 1 starts at index 0
+        // ((row1 - 1) * n = 0) then add the column you want
+        return (row - 1) * sideLength + col;
     }
+
     public void open(int row, int col) {
         if (isOpen(row, col)) {
-            return;
+            return; // No need to be opened already open
         }
 
         int[] indexes = new int[5];
         indexes[0] = toIndex(row, col);
+
+        // Instead of using virtual nodes, status is defined by index
         final int bottomRow = (sideLength - 1) * sideLength;
         if (indexes[0] <= sideLength) {
             status[indexes[0]] = TOP_CONN;
@@ -75,32 +67,31 @@ public class Percolation {
         }
         ++numOfOpenSites;
 
-        // Check for adjacent nodes, toIndex returns -1 if not found.
-        // Up
-        indexes[1] = toIndex(row - 1, col);
-        // Down
-        indexes[2] = toIndex(row + 1, col);
-        // Right
-        indexes[3] = toIndex(row, col + 1);
-        // Left
-        indexes[4] = toIndex(row, col - 1);
+        // Get adjacent nodes to compare status
+        indexes[1] = toIndex(row - 1, col); // Up
+        indexes[2] = toIndex(row + 1, col); // Down
+        indexes[3] = toIndex(row, col + 1); // Right
+        indexes[4] = toIndex(row, col - 1); // Left
 
-        byte statusGen = CLOSE;
+        // Merge roots adjacent status with bitwise OR
+        int newStatus = status[indexes[0]];
         for (int i : indexes) {
-            if (i != -1) {
-                statusGen = (byte) (statusGen | status[i]);
-                System.out.println(statusGen);
+            if (status[i] >= OPEN) { // Ignore CLOSED nodes (nullNode, etc.)
+                newStatus = newStatus | status[idsUF.find(i)];
             }
         }
 
+        // Creation of new tree from roots
         for (int i : indexes) {
-            if (i != -1 && status[i] != CLOSE) {
-                status[i] = statusGen;
+            if (status[i] >= OPEN) {
                 idsUF.union(i, indexes[0]);
             }
         }
 
-        if (status[indexes[0]] == PERCOLATION) {
+        // Update only the new root as we created a new huge tree
+        status[idsUF.find(indexes[0])] = newStatus;
+
+        if (newStatus == PERCOLATION) { // TOP_CONN and BOTTOM_CONN merged
             percolates = true;
         }
     }
@@ -116,14 +107,7 @@ public class Percolation {
 
     public boolean isFull(int row, int col) {
         enforceRange(row, col);
-        int i = toIndex(row, col);
-        boolean isFull = (status[i] >= OPEN) &&
-                (idsUF.find(topNode) == idsUF.find(i));
-        if (isFull && status[i] == BOTTOM_CONN) {
-            status[i] = PERCOLATION;
-        }
-
-        return isFull;
+        return (status[idsUF.find(toIndex(row, col))] & TOP_CONN) == TOP_CONN;
     }
 
     public boolean percolates() {
